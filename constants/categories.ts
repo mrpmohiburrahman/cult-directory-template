645c95bda0cbcf5508cb01f0ef127e4b08930051
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-11-21 12:14:54 +0600

    update type

--- constants/categories.ts
+++ constants/categories.ts
@@ -1,3 +1,30 @@
+export type ToolCategories = {
+  [key: string]: DeveloperTools
+}
+
+export type DeveloperTools = {
+  name: string
+  subcategories: Subcategory[]
+}
+
+export type Subcategory = {
+  name: string
+  deals: Deal[]
+}
+
+export type Deal = {
+  id: string
+  name: string
+  description: string
+  discount: string
+  link: string
+  logoSrc: string
+  category: string
+  subcategory: string
+  tags: string[]
+  labels: string[]
+  imageSrc: string
+}
 export const data = {
   DeveloperTools: {
     name: "👨‍💻 Developer Tools",
